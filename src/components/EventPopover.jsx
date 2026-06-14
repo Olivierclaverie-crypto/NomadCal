18e3a57c658bb85b94c5872f903b471345c79100
Claverie <olivierclaverie@me.com>
2026-06-14 15:56:21 +0200

    Update EventPopover.jsx

--- src/components/EventPopover.jsx
+++ src/components/EventPopover.jsx
@@ -112,7 +112,7 @@ export default function EventPopover({
               onClose();
             }}
           >
-            <CopyIcon />
+            <ClockIcon />
           </IconBtn>
 
           <IconBtn
